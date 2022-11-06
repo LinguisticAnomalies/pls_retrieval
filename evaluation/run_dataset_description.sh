198d830066d56fbffaac4d8bab3c0fdee493c0c6
Yue Guo <56052979+yueguo-50@users.noreply.github.com>
2022-11-06 14:31:43 -0800

    Delete run_dataset_description.sh

--- evaluation/run_dataset_description.sh
+++
@@ -1,7 +0,0 @@
-python dataset_description.py \
-    --data-path '/edata/yguo50/plain_language/pls/data/ordered_pairs_src_200_tgt_150_rouge_0.1-0.4_FixedROUGE_pairs/' \
-    --file-name 'full' \
-    --source-name whole.source \
-    --target-name whole.target 
-    # --source-name 'abs_text' \
-    # --target-name 'pls_text' 
