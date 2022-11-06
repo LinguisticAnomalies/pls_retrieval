e44628bc15e2f7066eb3e5ec9d42bccbb95ab506
Yue Guo <56052979+yueguo-50@users.noreply.github.com>
2022-11-06 15:10:02 -0800

    Update run_keywords.sh

--- preprocess/Wiki/run_keywords.sh
+++ preprocess/Wiki/run_keywords.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python keywords.py \
-    --data-path /edata/yguo50/plain_language/pls/data/src_100-1000_tgt_0-700_full_data_extract_elife_annals_medicine_reproductive/ \
+    --data-path ../data_path/ \
     --data-file test.csv \
     --num-keywords 3 \
     --source-name abs_text
