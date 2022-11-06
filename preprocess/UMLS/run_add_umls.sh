8a5eab0176d90f08b12b7af91dbd6aa459987659
Yue Guo <56052979+yueguo-50@users.noreply.github.com>
2022-11-06 15:02:10 -0800

    Update run_add_umls.sh

--- preprocess/UMLS/run_add_umls.sh
+++ preprocess/UMLS/run_add_umls.sh
@@ -1,5 +1,5 @@
 python add_umls_definition.py \
-    --data-path /edata/yguo50/plain_language/pls/data/full_data_extract_elife_annals_medicine_reproductive/ \
-    --save-path /edata/yguo50/plain_language/pls/data/full_data_extract_elife_annals_medicine_reproductive/add_umls/ \
+    --data-path ./data_path \
+    --save-path ./save_path/add_umls/ \
     --umls-file test_umls.json \
     --csv-file test.csv
