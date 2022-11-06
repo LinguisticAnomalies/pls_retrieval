a7a60a6d931f0535c68bdcafe25a94f89d0a4ac4
Yue Guo <56052979+yueguo-50@users.noreply.github.com>
2022-11-06 14:46:20 -0800

    Update run_test.sh

--- model/BART/run_test.sh
+++ model/BART/run_test.sh
@@ -1,8 +1,6 @@
 export CUDA_VISIBLE_DEVICES=1
-PREFIX=/edata/yguo50/plain_language/pls/data/ordered_pairs_src_200_tgt_150_rouge_0.1-0.4_FixedROUGE_pairs
-DATA_DIR=add_wiki
-MODEL_PATH=/edata/yguo50/plain_language/pls/output/pairs_lr_3e-05_${DATA_DIR}_bart_large_cnn
-DATA_PATH=$PREFIX/$DATA_DIR
+MODEL_PATH=../model_path
+DATA_PATH=../data_path
 LENPEN=1.0
 mkdir -p $MODEL_PATH/lenpen_$LENPEN
 cp ${DATA_PATH}/bin/dict.*  $MODEL_PATH
