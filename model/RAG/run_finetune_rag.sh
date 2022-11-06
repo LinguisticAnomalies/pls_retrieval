86c3c6019c8e0dec7897db51e87bd4d0393be155
Yue Guo <56052979+yueguo-50@users.noreply.github.com>
2022-11-06 14:51:31 -0800

    Update run_finetune_rag.sh

--- model/RAG/run_finetune_rag.sh
+++ model/RAG/run_finetune_rag.sh
@@ -2,15 +2,9 @@
 #!/bin/sh
 export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH="../":"${PYTHONPATH}"
-DATA_DIR="/edata/yguo50/plain_language/pls/data/ordered_pairs_src_200_tgt_150_rouge_0.1-0.4_FixedROUGE_background_before_2ndPair/"
-# DATA_DIR="/edata/yguo50/plain_language/pls/data/ordered_pairs_src_200_tgt_150_rouge_0.1-0.4_FixedROUGE_pairs/"
-OUTPUT_DIR="./rag_wiki_output_ordered_pairs_src_200_tgt_150_rouge_0.1-0.4_FixedROUGE_background_before_2ndPair_lr_1e-5_dropout_0.2"
-# OUTPUT_DIR="./rag_wiki_output_ordered_pairs_src_200_tgt_150_rouge_0.1-0.4_FixedROUGE_pairs"
-MODEL_NAME_OR_PATH="./rag_bart-large-cnn_checkpoint/"
-# MODEL_NAME_OR_PATH="/edata/yguo50/plain_language/pls/transformers/examples/research_projects/rag/huggingface_rag/rag_wiki_output_ordered_pairs_src_200_tgt_150_rouge_0.1-0.4_FixedROUGE_pairs/lr_5e-6_checkpoint2/"
-# MODEL_NAME_OR_PATH=/edata/yguo50/plain_language/Plain_language_summarization/checkpoints/pubmed_small_paper_pretrained_bart_large_cnndm/checkpoint_best.pt
-# A sample finetuning run, you need to specify data_dir, output_dir and model_name_or_path
-# run ./examples/rag/finetune_rag.sh --help to see all the possible options
+DATA_DIR="./data_dir"
+OUTPUT_DIR="./output_dir"
+MODEL_NAME_OR_PATH="./model_path/"
 
 python /edata/yguo50/plain_language/pls/transformers/examples/research_projects/rag/finetune_rag.py \
     --data_dir $DATA_DIR \
@@ -46,5 +40,3 @@ python /edata/yguo50/plain_language/pls/transformers/examples/research_projects/
 ##    --max_source_length 1000 \ # wiki: 800 source length + 200 retrieve length
 
 ### gradient_accumulation_steps: pairs-256, background-32
-
-# DATA_DIR="./full_data_extract_elife_annals_medicine_reproductive/"
