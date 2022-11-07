4c545d9b77e990e62da20067750a8c7b50e361a6
Yue Guo <56052979+yueguo-50@users.noreply.github.com>
2022-11-07 10:09:47 -0800

    Update run_test.sh

--- evaluation/plainess score/run_test.sh
+++ evaluation/plainess score/run_test.sh
@@ -5,7 +5,7 @@ mkdir ${MODEL_PATH}input0/
 cp ${DATA_PATH}-bin/input0/dict.*  ${MODEL_PATH}input0/
 python test.py \
   --model-dir $MODEL_PATH \
-  --model-file checkpoint31.pt \
+  --model-file checkpoint.pt \
   --data-path ${DATA_PATH}-bin/ \
   --bsz 256 \
   --src ${DATA_PATH}/test.input0 \
